3a4b6a8aad1a1a78ee8e12de702a3f42aa98f0a8
Jenny Kam <jkam17@gmail.com>
2019-06-27 22:03:47 -0400

    add template to routes, test using control logic in view

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello, World!"
+    user = {'username': 'Jenny'}
+    todos = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Go for a run!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Bake a cake'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, todos=todos)
