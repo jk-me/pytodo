8bf8a96adfb993f44987c8e602f1976f11f326b7
Jenny Kam <jkam17@gmail.com>
2019-06-28 15:41:06 -0400

    add config file with secret key, add flask-wtf for forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
+# app.config['SECRET_KEY'] = 'secret-key' as a dictionary/object
+# see in terminal by running `from pytodo import app` and `app.config['SECRET_KEY']`
 from app import routes
