d56f5e889fc6c0119bd117b8d88025dcc579cab9
Jenny Kam <jkam17@gmail.com>
2019-06-28 23:11:46 -0400

    add flask-sqlalchemy and flask-migrate, configure db setup and initialize migration repository

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 # app.config['SECRET_KEY'] = 'secret-key' as a dictionary/object
 # see in terminal by running `from pytodo import app` and `app.config['SECRET_KEY']`
-from app import routes
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
